5b9dbafeb7671be7c0967b84824d5b05d5b25981
Taka Fujita <fujita.it@gmail.com>
2015-04-10 18:17:09 +0200

    update battery_indicator.sh

--- bin/battery_indicator.sh
+++ bin/battery_indicator.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # modified from http://ficate.com/blog/2012/10/15/battery-life-in-the-land-of-tmux/
 
-HEART='♥ '
+# Set battery icon: '♥ '
+HEART='⦿ '
+# Set maximum number of battery indicators
+MAX_SLOTS=3
+# Set color (UTF-8 chars)
+color_active=colour181
+color_inactive=colour236
 
 if [[ `uname` == 'Linux' ]]; then
 	current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
@@ -11,16 +17,16 @@ else
 	current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
 	total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 fi
-
-charged_slots=$(echo "((($current_charge/$total_charge)*10)/3)+1" | bc -l | cut -d '.' -f 1)
-if [[ $charged_slots -gt 3 ]]; then
-	charged_slots=3
+# round up the charge with awk
+charged_slots=$(echo "(($current_charge/$total_charge)*$MAX_SLOTS)" | bc -l | awk '{printf("%d\n",$1 + 0.5)}')
+if [[ $charged_slots -gt $MAX_SLOTS ]]; then
+	charged_slots=$MAX_SLOTS
 fi
 
-echo -n '#[fg=colour213]'
+echo -n "#[fg=$color_active]"
 for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
 
-if [[ $charged_slots -lt 3 ]]; then
-	echo -n '#[fg=colour4]'
-	for i in `seq 1 $(echo "3-$charged_slots" | bc)`; do echo -n "$HEART"; done
+if [[ $charged_slots -lt $MAX_SLOTS ]]; then
+	echo -n "#[fg=$color_inactive]"
+	for i in `seq 1 $(echo "$MAX_SLOTS-$charged_slots" | bc)`; do echo -n "$HEART"; done
 fi
