6d91cf812faaa889e8b2c43eff172d561778e395
Taka Fujita <fujita.it@gmail.com>
2016-09-15 06:44:43 +0200

    Update

--- install/cask.sh
+++ install/cask.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-# Homebrew-cask app install
-
 #
 # Install Brew-cask packages.
 #
-
-# can treat another Java versions
 
 # add support for fonts
 brew tap caskroom/fonts
@@ -48,8 +44,8 @@ apps=(
   java6 # required for Arduino
 
   # Database 
-  mysqlworkbench
-  sequel-pro
+  # mysqlworkbench
+  # sequel-pro
   postgres
   pgadmin3
 
@@ -72,25 +68,15 @@ apps=(
   # libreoffice
 
   # Graphics / Music
-  sketch
-  sketchup
+  # sketch
+  # sketchup
   # pd-extended
   #cycling74-max # version 7 is available online
   audacity
 )
 
-# Install apps to /Applications
-# Default is: /Users/$user/Applications
 echo "installing apps..."
-# brew cask install --appdir="/Applications" ${apps[@]}
 brew cask install ${apps[@]}
-
-#
-# Link Cask Apps to Alfred
-#
-# brew cask alfred link # Does not work for free version
-# No need for Alfred 2.6 >
-
 
 #
 # Cleanup
